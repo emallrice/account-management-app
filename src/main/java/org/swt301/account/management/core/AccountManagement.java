921aafda2fb0a12b8c9c766cc0c81cdb6d640e5e
maotou-spy <phucbuihuu2604@gmail.com>
2023-03-01 22:40:03 +0700

    create new repository

---
+++ src/main/java/org/swt301/account/management/core/AccountManagement.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.swt301.account.management.core;
+
+/**
+ *
+ * @author Yemufu
+ */
+import java.util.ArrayList;
+
+public class AccountManagement {
+
+    private ArrayList<Account> accounts;
+
+    public AccountManagement() {
+        accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        Account accountById = getAccountById(account.getId());
+        if (accountById == null) {
+            accounts.add(account);
+        }
+    }
+
+    public void updateAccount(Account account) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getId() == account.getId()) {
+                accounts.set(i, account);
+                break;
+            }
+        }
+    }
+
+    public Account getAccountById(int id) {
+        for (Account acc : accounts) {
+            if (acc.getId() == id) {
+                return acc;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+}
---
+++ src/test/java/org/swt301/account/management/test/AccountManagementTest.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package org.swt301.account.management.test;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import org.swt301.account.management.core.Account;
+import org.swt301.account.management.core.AccountManagement;
+
+/**
+ *
+ * @author Yemufu
+ */
+
+public class AccountManagementTest {
+    private AccountManagement accountManager;
+
+    @BeforeTest
+    public void setUp() {
+        accountManager = new AccountManagement();
+    }
+
+    @Test
+    public void testAddAccount() {
+        Account account = new Account(1, "Alice", "alice@gmail.com", "123456789");
+        accountManager.addAccount(account);
+        Assert.assertEquals(accountManager.getAccountById(1), account);
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        Account account = new Account(1, "Alice", "alice@gmail.com", "123456789");
+        accountManager.addAccount(account);
+        account.setEmail("alice@yahoo.com");
+        accountManager.updateAccount(account);
+        Assert.assertEquals(accountManager.getAccountById(1).getEmail(), "alice@yahoo.com");
+    }
+
+    @Test
+    public void testGetAccountById() {
+        Account account = new Account(1, "Alice", "alice@gmail.com", "123456789");
+        accountManager.addAccount(account);
+        Assert.assertEquals(accountManager.getAccountById(1), account);
+    }
+
+    @Test
+    public void testGetAllAccounts() {
+        Account account1 = new Account(1, "Alice", "alice@yahoo.com", "123456789");
+        Account account2 = new Account(2, "Bob", "bob@gmail.com", "987654321");
+        accountManager.addAccount(account1);
+        accountManager.addAccount(account2);
+        Assert.assertEquals(accountManager.getAccounts().size(), 2);
+    }
+}
+
